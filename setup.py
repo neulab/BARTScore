e046f4545ed3395314e767254aacd123dbc16327
John Giorgi <johnmgiorgi@gmail.com>
2022-06-15 16:53:25 -0700

    Properly set up repo as package

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-
-setup(
+setuptools.setup(
     name="bart_score",
     version="0.1.0",
     description="BARTScore: Evaluating Generated Text as Text Generation",
     author="Weizhe Yuan",
     url="https://github.com/neulab/BARTScore",
-    packages=["bart_score"],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
     install_requires=[
         "torch>=1.6.0",
         "transformers>=4.6.1",
