e6c7ef6fe3147b9f065e22f92b1f3f05d3327eb1
John Giorgi <johnmgiorgi@gmail.com>
2022-06-15 16:05:20 -0700

    Replace requirements with setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='BARTScore',
+    version='0.1.0',
+    description='BARTScore: Evaluating Generated Text as Text Generation',
+    author='Weizhe Yuan',
+    url='https://github.com/neulab/BARTScore',
+    packages=[
+        'torch>=1.6.0',
+        'transformers>=4.6.1',
+    ],
+)
